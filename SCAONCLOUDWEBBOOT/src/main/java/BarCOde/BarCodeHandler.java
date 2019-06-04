e272bdc83f7029965cb39b1eac4fcdd219ea6036
Naman <Naman@192.168.1.10>
2019-06-04 13:20:24 +0530

    lll

--- SCAONCLOUDWEBBOOT/src/main/java/BarCOde/BarCodeHandler.java
+++ SCAONCLOUDWEBBOOT/src/main/java/BarCOde/BarCodeHandler.java
@@ -1,12 +1,10 @@
 package BarCOde;
 
 public class BarCodeHandler {
-<<<<<<< HEAD
+
 
 	public static void main(String [][] args) {
-=======
-	public static void main(String arrg[]) {
->>>>>>> refs/remotes/origin/Naman_PC
-		
-	}
+
+	
+}
 }
